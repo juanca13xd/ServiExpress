d30d575cc6605f3f561e88dd1e62182726f2e794
Juan Carlos <jcarlos132213@gmail.com>
2021-06-12 18:48:23 -0500

    Creación de primer Proyecto

---
+++ Sv_ServiExpress/src/main/java/com/serviexpress/SvServiExpressApplication.java
@@ -0,0 +1,13 @@
+package com.serviexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvServiExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvServiExpressApplication.class, args);
+	}
+
+}
---
+++ Sv_ServiExpress/src/test/java/com/serviexpress/SvServiExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvServiExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
